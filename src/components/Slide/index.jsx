cae481e2fa6ac891b1b9d0b7d034db552f9fde5b
Jonathan Phipps <jonathanmp25@gmail.com>
2019-02-07 15:42:46 -0600

    Fix null background image request bug

--- src/components/Slide/index.jsx
+++ src/components/Slide/index.jsx
@@ -5,34 +5,40 @@ import { Link } from 'react-router-dom';
 import transitionArrow from 'assets/right-arrow.svg';
 import './style.scss';
 
-const Slide = ({ style, image, index, prev, next, children }) => (
-  <animated.div className="slide" style={{ ...style, backgroundImage: `url(${image})`, zIndex: 10 + index }}>
-    <div className="arrow-cont">
-      { prev  &&
-        <Link to={prev}>
-          <img
-            className="arrow left"
-            src={transitionArrow}
-            alt="Go back arrow"
-          />
-        </Link>
-      }
-    </div>
-    <div className="page-content">
-      { children }
-    </div>
-    <div className="arrow-cont right-cont">
-      { next &&
-        <Link to={next}>
-          <img
-            className="arrow right"
-            src={transitionArrow}
-            alt="Next page arrow"
-          />
-        </Link>
-      }
-    </div>
-  </animated.div>
-);
+const Slide = ({ style, image, index, prev, next, children }) => {
+  let backgroundImage = '';
+  if (image) {
+    backgroundImage = `url(${image})`;
+  }
+  return (
+    <animated.div className="slide" style={{ ...style, backgroundImage, zIndex: 10 + index }}>
+      <div className="arrow-cont">
+        { prev  &&
+          <Link to={prev}>
+            <img
+              className="arrow left"
+              src={transitionArrow}
+              alt="Go back arrow"
+            />
+          </Link>
+        }
+      </div>
+      <div className="page-content">
+        { children }
+      </div>
+      <div className="arrow-cont right-cont">
+        { next &&
+          <Link to={next}>
+            <img
+              className="arrow right"
+              src={transitionArrow}
+              alt="Next page arrow"
+            />
+          </Link>
+        }
+      </div>
+    </animated.div>
+  );
+};
 
 export default Slide;
